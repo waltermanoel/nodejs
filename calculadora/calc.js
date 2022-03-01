60e2686a84e18832084f51b86e7942ce46c6e0d2
Walter Manoel <waltermanoel.pro@gmail.com>
2022-02-28 22:52:02 -0300

    Primeiros passos com módulos

---
+++ calculadora/calc.js
@@ -0,0 +1,9 @@
+var somar = require("./somar")
+var sub = require("./sub")
+var multi = require("./multi")
+var div = require("./div")
+
+console.log(somar(9, 4))
+console.log(sub(5, 3))
+console.log(multi(3, 9))
+console.log(div(40, 5))
---
+++ calculadora/multi.js
@@ -0,0 +1,5 @@
+var multi = function (n1, n2) {
+    return n1 * n2
+}
+
+module.exports = multi
