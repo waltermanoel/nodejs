c2d6cec7ed525f6a1424211bcc5a44956b4ee545
Walter Manoel <waltermanoel.pro@gmail.com>
2022-03-04 01:55:14 -0300

    Update teste.js

--- aula17/teste.js
+++ aula17/teste.js
@@ -9,3 +9,46 @@ sequelize.authenticate().then(function(){
 }).catch(function(erro){
     console.log("Falha ao se conectar: " + erro)
 })
+
+// Postagem 
+
+const Postagem = sequelize.define("postagens", {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+/*Postagem.create({
+    titulo: "HTML",
+    conteudo: "HTML é uma liguagem de marcação para Web."
+})*/
+
+
+// Usuários
+
+const Usuario = sequelize.define("usuarios", {
+    nome: {
+        type: Sequelize.STRING
+    }, 
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    }, 
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+Usuario.create({
+    nome: "Walter",
+    sobrenome: "Manoel",
+    idade: 17,
+    email: "waltermanoel@gamil.com"
+})
+
+// Usuario.sync({force: true})
